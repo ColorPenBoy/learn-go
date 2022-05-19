d4af5492df1357a877d0713db5464dd0e51fcdbf
张强 <zq252125@163.com>
2022-05-19 20:57:01 +0800

    update

--- 02-go-gin/main.go
+++ 02-go-gin/main.go
@@ -2,31 +2,28 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/topic/:topic_id", func(c *gin.Context) {
-		c.String(http.StatusOK, "获取帖子ID为%s", c.Param("topic_id"))
-	})
+	v1 := router.Group("/v1/topics")
+	// 此处可以插入各种逻辑，只是使用代码块增加可读性
+	{
+		// localhost:8080/v1/topics?username=colorpen
+		v1.GET("", func(c *gin.Context) {
+			if c.Query("username") == "" {
+				c.String(200, "获取帖子列表")
+			} else {
+				c.String(200, "获取用户名=%s的帖子列表", c.Query("username"))
+			}
+		})
 
-	router.GET("/topic/top", func(c *gin.Context) {
-		c.String(http.StatusOK, "TOP")
-	})
-
-	router.GET("/v1/topics", func(c *gin.Context) {
-		if c.Query("username") == "" {
-			c.String(200, "获取帖子列表")
-		} else {
-			c.String(200, "获取用户名=%s的帖子列表", c.Query("username"))
-		}
-	})
-
-	router.GET("/v1/topics/:topic_id", func(c *gin.Context) {
-		c.String(200, "获取topicid=%s的帖子", c.Param("topic_id"))
-	})
+		// localhost:8080/v1/topics/13
+		v1.GET("/:topic_id", func(c *gin.Context) {
+			c.String(200, "获取topicid=%s的帖子", c.Param("topic_id"))
+		})
+	}
 
 	router.Run()
 }
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Golang Learning
 
 01 - GIN - 路由URL设计、参数
 02 - GIN - 路由分组
-
+03 - GIN - DAO层代码封装、使用中间件模拟鉴权
 
 
 
