68178d8f579208286327fa7f3aad923bca0853be
张强 <zq252125@163.com>
2022-05-21 19:27:49 +0800

    09 - GORM 与 model映射 sql查询方法

---
+++ 09-go-gin/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
+	"os"
+	"test-go/model"
+	"time"
+)
+
+// 新版GORM写法
+func main() {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/test_go?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			//TablePrefix:   "t_",                              // table name prefix, table for `User` would be `t_users`
+			SingularTable: true, // 1、默认gorm会在表名后面加复数，设置这个参数后，即可关闭
+			//NoLowerCase:   true,                              // skip the snake_casing of names
+			//NameReplacer:  strings.NewReplacer("CID", "Cid"), // use name replacer to change struct/field name before convert it to db name
+		},
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags),
+			logger.Config{
+				SlowThreshold:             time.Second,
+				LogLevel:                  logger.Info,
+				IgnoreRecordNotFoundError: true,
+				Colorful:                  true,
+			},
+		), //加SQL的执行Log
+	})
+	if db == nil || err != nil {
+		fmt.Println("数据库连接错误: ", err.Error())
+	}
+	sqlDB, _ := db.DB()
+	defer sqlDB.Close()
+
+	// 单条查询
+	tc := model.TopicClass{}
+	// 3、此处也可以直接指定表名
+	db.Table("topic_class").First(&tc, 2)
+	fmt.Println(tc)
+
+	// 多条查询
+	var tcs []model.TopicClass
+	db.Find(&tcs)
+	fmt.Println(tcs)
+
+	// where查询
+	var tcs2 []model.TopicClass
+	// db.Where("class_name=?", "技术类").Find(&tcs2)
+	db.Where(&model.TopicClass{ClassName: "技术类"}).Find(&tcs2)
+	fmt.Println(tcs2)
+}
+
+////旧版GORM写法
+//import (
+//	"fmt"
+//	"github.com/jinzhu/gorm"
+//	_ "github.com/jinzhu/gorm/dialects/mysql"
+//)
+//
+//func main() {
+//	db, err := gorm.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/test_go?charset=utf8mb4&parseTime=True&loc=Local")
+//	if db == nil || err != nil {
+//		fmt.Println("数据库连接错误: ", err.Error())
+//	}
+//	defer db.Close()
+//
+//	rows, _ := db.Raw("SELECT topic_id, topic_title FROM topics").Rows()
+//	for rows.Next() {
+//		var t_id int
+//		var t_title string
+//		rows.Scan(&t_id, &t_title)
+//		fmt.Println(t_id, t_title)
+//	}
+//}
+
+/*
+	参数验证框架：
+		https://github.com/go-playground/validator
+	文档：
+		https://godoc.org/gopkg.in/go-playground/validator.v9
+*/
+//func main() {
+//	router := gin.Default()
+//	// 注册参数验证器
+//	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+//		err1 := v.RegisterValidation("topicurl", myvalidator.TopicUrl)
+//		err2 := v.RegisterValidation("topiclist", myvalidator.TopicList)
+//		if err1 != nil || err2 != nil {
+//			return
+//		}
+//	}
+//
+//	// 单条帖子
+//	v1 := router.Group("/v1/topics")
+//
+//	// 此处可以插入各种逻辑，只是使用代码块增加可读性
+//	{
+//		// localhost:8080/v1/topics?username=colorpen
+//		v1.GET("", dao.GetTopicList)
+//
+//		// localhost:8080/v1/topics/13
+//		v1.GET("/:topic_id", dao.GetTopicDetail)
+//
+//		// 如果使用了中间件，下面的所有路由都需要中间件，进行token判断
+//		v1.Use(dao.MustLogin())
+//		{
+//			// 新增Topic
+//			v1.POST("/add", dao.NewTopic)
+//
+//			// 删除Topic
+//			v1.DELETE("/del/:topic_id", dao.DelTopic)
+//		}
+//	}
+//
+//	// 多条帖子批量处理
+//	v2 := router.Group("/v1/multi/topics")
+//	// 此处可以插入各种逻辑，只是使用代码块增加可读性
+//	{
+//		// 如果使用了中间件，下面的所有路由都需要中间件，进行token判断
+//		v2.Use(dao.MustLogin())
+//		{
+//			// 新增多条Topics
+//			v2.POST("/add", dao.NewTopicBatch)
+//		}
+//	}
+//
+//	router.Run()
+//}
---
+++ 09-go-gin/model/TopicModel.go
@@ -0,0 +1,31 @@
+package model
+
+type Topic struct {
+	TopicID int `json:"id"`
+	// 标题长度必须在4-20位
+	TopicTitle string `json:"title" binding:"min=4,max=20"`
+	// 短标题和标题不能一样
+	TopicShortTitle string `json:"shortTitle" binding:"required,nefield=TopicTitle"`
+	// 可以不填，如果填写，需要符合 `topicurl` 的规则
+	TopicUrl string `json:"url" binding:"omitempty,topicurl"`
+	// user ip必须是ipv4的形式
+	UserIP string `json:"ip" binding:"ipv4"`
+	// Score要么不填，要填必须大于5分
+	TopicScore int `json:"score" binding:"omitempty,gt=5"`
+}
+
+type Topics struct {
+	// 添加dive，进行嵌套验证
+	TopicList     []Topic `json:"topics" binding:"gt=0,lt=3,topiclist,dive"`
+	TopicListSize int     `json:"size"`
+}
+
+func CreateTopic(id int, title string) Topic {
+	return Topic{id, title, "", "", "", 0}
+}
+
+type TopicQuery struct {
+	UserName string `json:"username" form:"username" binding:"required"` // 必填参数
+	Page     int    `json:"page" form:"page"`
+	PageSize int    `json:"pagesize" form:"pagesize"`
+}
